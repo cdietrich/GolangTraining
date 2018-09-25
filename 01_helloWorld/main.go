571ff1f11f5a0486036769f129d66c9653217d8c
Christian Dietrich <christian.dietrich@itemis.de>
2018-09-25 18:03:28 +0200

    more stuff
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello, World")
+	fmt.Println(42)
+	fmt.Printf("%d %b %x %#x %X %#X %o %#o\n", 42, 42, 42, 42, 42, 42, 42, 42)
+	fmt.Println("====================")
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d\t%b\t%#x\t%q\n", i, i, i, i)
+	}
 }
