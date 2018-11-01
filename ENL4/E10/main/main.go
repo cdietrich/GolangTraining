a68966f853e6d7f348db10e201832e636e977602
Christian Dietrich <christian.dietrich@itemis.de>
2018-11-01 12:09:44 +0100

    more stuff
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- ENL4/E10/main/main.go
+++ ENL4/E10/main/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println()
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	delete(m, `no_dr`)
+	for person, things := range m {
+		fmt.Println("Person:", person)
+		for i, v := range things {
+			fmt.Println(i, v)
+		}
+	}
 }
