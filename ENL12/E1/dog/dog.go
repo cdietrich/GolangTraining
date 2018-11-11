ed6727f198bfdd1a57cb2113f6958ee82c603337
Christian Dietrich <christian.dietrich@itemis.de>
2018-11-11 20:33:07 +0100

    enl12
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ ENL12/E1/dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog is a nice package about dogs
+package dog
+
+// Years converts the given human age into a dogs age
+func Years(human int) int {
+	return human * 7
+}
---
+++ ENL12/E1/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cdietrich/GoLangTraining/ENL12/E1/dog"
+)
+
+func main() {
+	fmt.Println(dog.Years(5))
+}
