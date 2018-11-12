fef6f9628bc8d3ba893386a02ca00b56060fcb16
Christian Dietrich <christian.dietrich@itemis.de>
2018-11-12 19:20:45 +0100

    doc example
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ 22_documentation/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cdietrich/GoLangTraining/22_documentation/myutil"
+)
+
+func main() {
+	fmt.Println(myutil.DoSth(1, 2))
+}
---
+++ 22_documentation/myutil/myutil.go
@@ -0,0 +1,7 @@
+// Package myutil is a real useful package to deal with stuff
+package myutil
+
+// DoSth is a real nice function that return a*10+b
+func DoSth(a, b int) int {
+	return a*10 + b
+}
